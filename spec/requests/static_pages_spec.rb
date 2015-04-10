057d5affeea6d51aaedfdf0ce0a64b4e700af190
nhosoya <hosoya.naoki@icloud.com>
2015-04-11 00:10:47 +0900

    Add contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,7 +8,7 @@ describe "Static Pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
-    
+
     it "should have the title 'Home'" do
       visit '/static_pages/home'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
@@ -21,7 +21,7 @@ describe "Static Pages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
-    
+
     it "should have the title 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
@@ -38,6 +38,19 @@ describe "Static Pages" do
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end
+
+    describe "Contact" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 end
